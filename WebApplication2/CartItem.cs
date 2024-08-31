7ce91472654026650c792ffd6f5e34b1147d0dc4
EckartWolfaardt <eckartwolfaardt@gmail.com>
2024-08-31 22:27:28 +0200

    Add files via upload
    All files related to the webapp

---
+++ WebApplication2/CartItem.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace EcommerceSite.CartItem
+{
+    public class CartItem
+    {
+        public int Id { get; set; }
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ WebApplication2/DataBase.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+
+namespace ProductDataBase
+{
+    public class Product
+    {
+        private readonly SqlConnection conn;
+
+        // Add product and delete pruduct
+        public void insertProduct(Products product)
+        {
+            using (SqlConnection connection = new SqlConnection())
+            {
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = "INSERT INTO [Products](ProductID, ProductName, ProductPrice, ProductStock, ProductDescription) VALUES(@ID, @Name, @Price, @Stock, @Description)";
+
+                    command.Parameters.AddWithValue("@ID", product.productID);
+                    command.Parameters.AddWithValue("@Name", product.productName);
+                    command.Parameters.AddWithValue("@Price", product.productPrice);
+                    command.Parameters.AddWithValue("@Stock", product.productStock);
+                    command.Parameters.AddWithValue("@Description", product.productDescription);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    Console.WriteLine("Product Added");
+                    connection.Close();
+                }
+            }
+        }
+
+        public void deleteProduct(string ID)
+        {
+            using (SqlConnection connection = new SqlConnection())
+            {
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = "DELETE FROM Products WHERE ProductID = @ID";
+                    connection.Open();
+                    command.Parameters.AddWithValue("@ID", ID);
+                    command.ExecuteNonQuery();
+                }
+                connection.Close();
+            }
+        }
+        // add and delete users
+        public void insertUser(Users users)
+        {
+            using (SqlConnection connection = new SqlConnection())
+            {
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = "INSERT INTO [Users](UserID, UserName, UserSurname, UserUsername, UserPassword) VALUES(@ID, @Name, @Surname, @Username, @Password)";
+
+                    command.Parameters.AddWithValue("@ID", users.UserID);
+                    command.Parameters.AddWithValue("@Name", users.UserName);
+                    command.Parameters.AddWithValue("@Price", users.UserSurname);
+                    command.Parameters.AddWithValue("@Stock", users.UserUsername);
+                    command.Parameters.AddWithValue("@Description", users.USerPassword);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    Console.WriteLine("User Added");
+                    connection.Close();
+                }
+            }
+        }
+
+        public void deleteUser(string ID)
+        {
+            using (SqlConnection connection = new SqlConnection())
+            {
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = "DELETE FROM Users WHERE UserID = @ID";
+                    connection.Open();
+                    command.Parameters.AddWithValue("@ID", ID);
+                    command.ExecuteNonQuery();
+                }
+                connection.Close();
+            }
+        }
+    }
+}
